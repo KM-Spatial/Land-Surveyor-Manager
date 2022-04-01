f47ef5c69d4143796802f07b2cf2a5c5fd74c298
Surveyor-Jr <matingonk@gmail.com>
2022-04-01 09:13:39 +0200

    fixed errors ready to share now

--- users/signals.py
+++ users/signals.py
@@ -1,9 +1,12 @@
+import random
+import string
+from datetime import datetime, timedelta, date
+
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile, Billing
 
-from datetime import datetime, timedelta, date
+from .models import Profile, Billing
 
 
 # Signal To Create a User-Profile on Account Creation
@@ -22,12 +25,14 @@ def save_user_profile(sender, instance, **kwargs):
 @receiver(post_save, sender=User)
 def create_free_trial(sender, instance, created, **kwargs):
     if created:
-        Billing.objects.create(user=instance, email=instance, phone='0776887606',
+        Billing.objects.create(user=instance, email=instance, phone='0718870810',
                                paid_on=datetime.now(),
                                amount=0,
                                payment_method='ecocash',
                                expire_date=date.today() + timedelta(days=15),  # -> Assign 7 Day Trial
-                               reference_code='free_trial'
+                               reference_code='free_trial'.join(random.choices(string.ascii_lowercase + string.digits,
+                                                                               k=5))
+                               # Assign a 5-digit code at the end
                                )
 
 
