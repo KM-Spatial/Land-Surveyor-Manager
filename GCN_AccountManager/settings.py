01c40e74a51ee61c610cd3ed7e2d4901a78c5001
Surveyor Jr <matingonk@gmail.com>
2025-01-12 15:11:52 +0200

    DB now using PostgreSQL

--- GCN_AccountManager/settings.py
+++ GCN_AccountManager/settings.py
@@ -97,10 +97,21 @@ WSGI_APPLICATION = 'GCN_AccountManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'HOST': env('DB_HOST'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'PORT': '5432',
     }
 }
 
